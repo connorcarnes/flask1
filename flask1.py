8b94194da0753570f477aa18f74a7b8a4f80230f
connorcarnes <connorcarnes44@gmail.com>
2020-11-26 20:52:21 +0000

    started login and registration functionality

--- flask1.py
+++ flask1.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'c4cf3bbdb8fdc824c052edcf7ce74eb9'
 
 posts = [
     {
@@ -23,11 +26,25 @@ posts = [
 def home():
     return render_template('home.html', posts=posts)
     
+    
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
     
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+    
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+    
 
 if __name__ == '__main__':
-    app.run(debug=True,host='localhost',port=8080)
+    app.run(debug=True,host='0.0.0.0',port=8080)
     
