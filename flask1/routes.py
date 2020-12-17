4cd4d40927837612e5b82cfeddff4eec937dee3c
connorcarnes <connorcarnes44@gmail.com>
2020-12-17 02:24:54 +0000

    started account page, vid7@27:04

--- flask1/routes.py
+++ flask1/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from flask1 import app, db, bcrypt
-from flask1.forms import RegistrationForm, LoginForm
+from flask1.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask1.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
     
@@ -31,7 +31,8 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
-    
+
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -46,6 +47,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
     
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -61,15 +63,29 @@ def login():
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
     
+
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
-    
-@app.route("/account")
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-        return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                            image_file=image_file, form=form)
 
     
     
