0289411a5dd0b9794ce97b6e3e71bd61e8606dce
connorcarnes <connorcarnes44@gmail.com>
2020-12-22 04:16:46 +0000

    Added functionality for logged in users to create posts, vid8@18:05

--- flask1/routes.py
+++ flask1/routes.py
@@ -3,31 +3,15 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flask1 import app, db, bcrypt
-from flask1.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flask1.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask1.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-    
-
-posts = [
-    {
-        'author': 'Connor Carnes',
-        'title':  'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Kenneth Carnes',
-        'title':  'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 30, 2018'
-    }
-    
-]
 
 
 @app.route("/")     # example of multiple routes
 @app.route("/home") # example of multiple routes
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
     
     
@@ -106,9 +90,18 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                             image_file=image_file, form=form)
+                            
 
-    
-    
-    
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
     
     
